734058fa2421f2a8bb22b18a7ba65535395570ee
Industralium <sandermaddison@gmail.com>
2022-04-03 10:40:53 +0300

    Initial commit

---
+++ src/main/java/com/example/helmesback/HelmesBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.helmesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelmesBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelmesBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helmesback/HelmesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helmesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelmesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
